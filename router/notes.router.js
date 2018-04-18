6d622022bb35dd4d16aea3508af998a78634c849
Cam Prier <CSPrier@gmail.com>
2018-04-18 14:59:57 -0700

    Added router.post method to notes.router.js

--- router/notes.router.js
+++ router/notes.router.js
@@ -68,6 +68,34 @@ router.put('/notes/:id', (req, res, next) => {
   });
 });
 
+// ==== POST =================================================
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+
+  /***** NEVER TRUST USERS - Validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
+// ==== DELETE =================================================
+
 
 // ERROR HANDLING =============================================
 router.use(function (req, res, next) {
