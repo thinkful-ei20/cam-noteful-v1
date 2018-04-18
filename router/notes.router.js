bd97abe2f1d3d272409efc978302c039eade36df
Cam Prier <CSPrier@gmail.com>
2018-04-18 14:53:31 -0700

    Added router functionality and tested on POSTMAN. Requests work fine

---
+++ router/notes.router.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+const data = require('../db/notes'); // Database
+const simDB = require('../db/simDB');  // Database methods
+const notes = simDB.initialize(data);
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+
+// REQUESTS =============================================
+// ==== GET =================================================
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+  notes.filter(searchTerm, (err, list) => {
+    if (list.length === 0 || err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+router.get('/notes/:id', (req, res, next) => {
+  const { id } = req.params;
+  notes.find(id, (err, item) => {
+    if (item === undefined || err) { 
+      return next(err);
+    }
+    res.json(item); 
+  });
+});
+
+
+// ==== PUT =================================================
+// Use the PUT method on the route api/notes/:id, then use the callback function with 3 params, req ,res, and next
+router.put('/notes/:id', (req, res, next) => {
+  // Initialize the ID variable as req.params.id
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  /// Loop over updateFields, and for Each element in the array
+  updateFields.forEach(field => {
+    // if field is IN(or it's prototype chain) the request body (returns true)
+    if (field in req.body) {
+      // updateObj[field] is initialized as request.body[field]
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  // ==== UPDATE =================================================
+  // update method on notes
+  notes.update(id, updateObj, (err, item) => {
+    if (Object.keys(updateObj).length === 0 || err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
+// ERROR HANDLING =============================================
+router.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+router.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 'use strict';
-/*global express*/
 
 console.log('Hello Noteful!');
-
 
 // import express
 const express = require('express');
@@ -10,13 +8,16 @@ const morgan = require('morgan'); // import morgan logger
 
 // Import data
 // Simple In-Memory DataBase
-const data = require('./db/notes'); // Database
-const simDB = require('./db/simDB');  // Database methods
-const notes = simDB.initialize(data);
+// const data = require('./db/notes'); // Database
+// const simDB = require('./db/simDB');  // Database methods
+// const notes = simDB.initialize(data);
 
 
 // Import the PORT module
 const { PORT } = require('./config');
+
+// Import ROUTER
+const notesRouter = require('./router/notes.router.js');
 
 // Init a new express app
 const app = express();
@@ -30,73 +31,9 @@ app.use(express.static('public'));
 // Parse request body
 app.use(express.json());
 
-// Create a GET endpoint which accepts an ID
-// NORMAL ROUTES
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-  notes.filter(searchTerm, (err, list) => {
-    if (list.length === 0 || err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
+// Mount the router
+app.use('/api', notesRouter);
 
-app.get('/api/notes/:id', (req, res, next) => {
-  const { id } = req.params;
-  notes.find(id, (err, item) => {
-    if (item === undefined || err) { 
-      return next(err);
-    }
-    res.json(item); 
-  });
-});
-
-// Use the PUT method on the route api/notes/:id, then use the callback function with 3 params, req ,res, and next
-app.put('/api/notes/:id', (req, res, next) => {
-  // Initialize the ID variable as req.params.id
-  const id = req.params.id;
-
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  /// Loop over updateFields, and for Each element in the array
-  updateFields.forEach(field => {
-    // if field is IN(or it's prototype chain) the request body (returns true)
-    if (field in req.body) {
-      // updateObj[field] is initialized as request.body[field]
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  // update method on notes
-  notes.update(id, updateObj, (err, item) => {
-    if (Object.keys(updateObj).length === 0 || err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-// ERROR HANDLING
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.status(404).json({ message: 'Not Found' });
-});
-
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err,
-  });
-});
 
 // Starts the server and listens on PORT variable
 app.listen(PORT, () => {
