cc700d06936b4f9e8d75d3bc3aa0400ca72fbb87
Cam Prier <CSPrier@gmail.com>
2018-04-17 17:21:24 -0700

    Added app.put error handling and functionality

--- server.js
+++ server.js
@@ -19,17 +19,20 @@ const { PORT } = require('./config');
 const { logger } = require('./public/middleware/logger.js');
 // Init a new express app
 const app = express();
-// Tell node to use the express.static built-in middleware
-app.use(express.static('public'));
+
 // Use the logger module
 app.use(logger);
+// Tell node to use the express.static built-in middleware
+app.use(express.static('public'));
+// Parse request body
+app.use(express.json());
 
 // Create a GET endpoint which accepts an ID
 // NORMAL ROUTES
 app.get('/api/notes', (req, res, next) => {
   const { searchTerm } = req.query;
   notes.filter(searchTerm, (err, list) => {
-    if (searchTerm === undefined || err) {
+    if (list.length === 0 || err) {
       return next(err); // goes to error handler
     }
     res.json(list); // responds with filtered array
@@ -46,9 +49,36 @@ app.get('/api/notes/:id', (req, res, next) => {
   });
 });
 
-// app.get('/boom', (req, res, next) => {
-//   throw new Error('Boom!');
-// });
+// Use the PUT method on the route api/notes/:id, then use the callback function with 3 params, req ,res, and next
+app.put('/api/notes/:id', (req, res, next) => {
+  // Initialize the ID variable as req.params.id
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  /// Loop over updateFields, and for Each element in the array
+  updateFields.forEach(field => {
+    // if field is IN(or it's prototype chain) the request body (returns true)
+    if (field in req.body) {
+      // updateObj[field] is initialized as request.body[field]
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  // update method on notes
+  notes.update(id, updateObj, (err, item) => {
+    if (Object.keys(updateObj).length === 0 || err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
 
 // ERROR HANDLING
 app.use(function (req, res, next) {
