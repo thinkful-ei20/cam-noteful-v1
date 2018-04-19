7d00b2a2510ea15ae363d740872cd2e8592bbbf7
Cam Prier <CSPrier@gmail.com>
2018-04-19 14:45:40 -0700

    Added filter promise functionality to the first get request in notes.router.js

--- router/notes.router.js
+++ router/notes.router.js
@@ -19,12 +19,23 @@ const jsonParser = bodyParser.json();
 // ==== GET =================================================
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
-  notes.filter(searchTerm, (err, list) => {
-    if (list.length === 0 || err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
+  // notes.filter(searchTerm, (err, list) => {
+  //   if (list.length === 0 || err) {
+  //     return next(err); // goes to error handler
+  //   }
+  //   res.json(list); // responds with filtered array
+  // });
+
+  notes.filter(searchTerm)
+    .then(list => {
+      if (list) {
+        res.json(list);
+      } else {
+        next();
+      }
+    }).catch(err => {
+      return next(err);
+    });
 });
 
 router.get('/notes/:id', (req, res, next) => {
